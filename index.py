11fa5eac8403cb7cc28c4001d1658e2c69754961
sukmin16 <162937112+sukmin16@users.noreply.github.com>
2024-04-05 14:30:05 +0900

    Add files via upload

---
+++ index.py
@@ -0,0 +1,21 @@
+#변수 자료를 담는 공간
+#예약어 if for True False
+#함수  자주사용하는 기능 내장함수 print() type()
+#연산자 산술 + - 대입 = 비교(관계) > < 논리(and or not)
+#자료형 정수int 실수float 문자 str
+#이스케이스코드 43 줄바꿈 \n
+#문자열 다루기 인덱싱
+
+n = "Happy Programming!"
+print(n)
+m = n[0:5]
+print(m)
+print(n[-12:-8])
+
+print(len(n)) #글자수를 세준다(len)
+print(n.upper())
+name = "제이름은 장석민입니다."
+name_e ="my name is sukmin"
+print(name_e.upper())
+print(len(name_e))
+
